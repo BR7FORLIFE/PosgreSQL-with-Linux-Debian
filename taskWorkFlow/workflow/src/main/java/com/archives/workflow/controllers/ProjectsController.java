a711a204e282fddb70523f9784b64ca62763b0bd
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-06-23 20:50:20 -0500

    better refactory and features

--- taskWorkFlow/workflow/src/main/java/com/archives/workflow/controllers/ProjectsController.java
+++ taskWorkFlow/workflow/src/main/java/com/archives/workflow/controllers/ProjectsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.archives.workflow.constants.ProjectResponseEntity;
 import com.archives.workflow.models.Projects;
 import com.archives.workflow.services.ProjectService;
 
@@ -35,29 +36,48 @@ public class ProjectsController {
     // crear un proyecto segun la client_id y cuerpo del projecto a agregar
     @PostMapping("/project/create/{client_id}")
     public ResponseEntity<String> createProject(@PathVariable UUID client_id, @RequestBody Projects projects) {
+        ProjectResponseEntity.uuidValidate(client_id, projects, HttpStatus.CREATED, "Project has been created!!");
         projectService.createProject(client_id, projects);
         return ResponseEntity.status(HttpStatus.CREATED).body("Project Created Succesfull for the Client!");
     }
 
+    //hay que mejorar los dos returns de los metodos el del estatico y de este metodo!
     @GetMapping("/project/read/{Client_id}")
     public ResponseEntity<List<Projects>> getAllProjectForUser(@PathVariable UUID client_id) {
+        ProjectResponseEntity.uuidValidate(client_id, HttpStatus.OK, List.of(new Projects()));
         List<Projects> allProjects = projectService.getAllProjectForUser(client_id);
-        return ResponseEntity.status(HttpStatus.OK).body(allProjects);
+        return null;
     }
 
     @GetMapping("/project/read/{client_id}/{id}")
-    public ResponseEntity<Projects> getProjectById(@PathVariable UUID client_id, int id){
+    public ResponseEntity<Projects> getProjectById(@PathVariable UUID client_id, Integer id) {
+        if (!(client_id instanceof UUID))
+            return ResponseEntity.badRequest().body(new Projects());
+        if (!(id instanceof Integer))
+            return ResponseEntity.badRequest().body(new Projects());
         Projects project = projectService.getProjectById(id);
         return ResponseEntity.status(HttpStatus.OK).body(project);
     }
 
+    // TODo - debo verificar la id del proyecto si existe!
     @PutMapping("/project/update/{client_id}/{id}")
-    public void editProject(){
-        
+    public ResponseEntity<String> editProject(@PathVariable UUID client_id, Integer id, @RequestBody Projects project) {
+        if (!(client_id instanceof UUID))
+            return ResponseEntity.badRequest().body("Expected UUID for client_id");
+        if (!(id instanceof Integer))
+            return ResponseEntity.badRequest().body("Expected id of project instanceof Integer!");
+        if (!(project == null))
+            return ResponseEntity.badRequest().body("The project is null expected object full!");
+        projectService.editProject(client_id, id, project);
+        return ResponseEntity.ok().body("Project has been editing!");
     }
 
     @DeleteMapping("/project/delete/{client_id}/{id}")
-    public void deleteProject(int id){
-
+    public ResponseEntity<String> deleteProject(@PathVariable UUID client_id, @PathVariable Integer id) {
+        if (!(client_id instanceof UUID))
+            return ResponseEntity.badRequest().body("Client_id not UUID");
+        if (!(id instanceof Integer))
+            return ResponseEntity.badRequest().body("The id the project not Integer!");
+        return ResponseEntity.ok().body("Project remove succesfull!");
     }
 }
