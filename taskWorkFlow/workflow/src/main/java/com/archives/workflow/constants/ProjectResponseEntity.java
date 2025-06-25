837f12b8a19c51f98d70ba8cef5c5f36a43249c8
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-06-24 19:29:02 -0500

    add multiples functionalitys

--- taskWorkFlow/workflow/src/main/java/com/archives/workflow/constants/ProjectResponseEntity.java
+++ taskWorkFlow/workflow/src/main/java/com/archives/workflow/constants/ProjectResponseEntity.java
@@ -9,36 +9,114 @@ import com.archives.workflow.models.Projects;
 
 public class ProjectResponseEntity {
 
-    public static <T> ResponseEntity<?> uuidValidate(UUID client_id, HttpStatus httpStatus,
-            T returnType) {
-        if (!(client_id instanceof UUID))
-            return ResponseEntity.badRequest().body("Expected UUID for client_id");
-        switch (httpStatus) {
-            case CREATED -> {
-                return ResponseEntity.status(HttpStatus.CREATED).body(returnType);
-            }
-            case OK -> {
-                return (ResponseEntity<?>) ResponseEntity.ok();
-            }
-            default -> throw new AssertionError();
-        }
+    /**
+     * 
+     * @param <T>        generico que nos permira saber que tipo de dato sera
+     *                   retornado en el cuerpo de la peticion
+     * @param client_id  id del cliente en la base de datos
+     * @param id         id del proyecto en la base de datos
+     * @param httpStatus status code que sera retornado al cliente ej: 200, 203, 404
+     *                   , 500 etc.
+     * @param returnType mensaje de retorno si las validaciones salieron correcto
+     * @param error      mensaje de error si las validaciones retornaron bad request
+     * @return retorna error / cuerpo - returntype / cuerpo
+     */
+    public static <T> ResponseEntity<T> Validate(UUID client_id, Integer id, HttpStatus httpStatus, T returnType,
+            T error) {
+        if (client_id == null)
+            return ResponseEntity.badRequest().body(error);
+        if (id instanceof Integer)
+            return ResponseEntity.ok().body(error);
+
+        return switch (httpStatus) {
+            case CREATED -> ResponseEntity.status(HttpStatus.CREATED).body(returnType);
+            case OK -> ResponseEntity.ok(returnType);
+            default -> throw new IllegalArgumentException("Unsupported HTTP status");
+        };
     }
 
-    public static <T> ResponseEntity<?> uuidValidate(UUID client_id, Projects projects, HttpStatus httpStatus,
-            T returnType) {
-        if (!(client_id instanceof UUID))
-            return ResponseEntity.badRequest().body("Expected UUID for client_id");
-        if (projects == null)
-            return ResponseEntity.badRequest().body("The project is null expected object full!");
+    /**
+     * 
+     * @param <T>        generico que nos permira saber que tipo de dato sera
+     *                   retornado en el cuerpo de la peticion
+     * @param client_id  id del cliente en la base de datos
+     * @param httpStatus status code que sera retornado al cliente ej: 200, 203, 404
+     *                   , 500 etc.
+     * @param returnType mensaje de retorno si las validaciones salieron correcto
+     * @param error      mensaje de error si las validaciones retornaron bad request
+     * @return retorna error / cuerpo - returntype / cuerpo
+     */
+    public static <T> ResponseEntity<T> Validate(UUID client_id, HttpStatus httpStatus, T returnType,
+            T error) {
+        if (client_id == null)
+            return ResponseEntity.badRequest().body(error);
 
-        switch (httpStatus) {
-            case CREATED -> {
-                return ResponseEntity.status(HttpStatus.CREATED).body(returnType);
-            }
-            case OK -> {
-                return (ResponseEntity<?>) ResponseEntity.ok();
-            }
-            default -> throw new AssertionError();
-        }
+        return switch (httpStatus) {
+            case CREATED -> ResponseEntity.status(HttpStatus.CREATED).body(returnType);
+            case OK -> ResponseEntity.ok(returnType);
+            default -> throw new IllegalArgumentException("Unsupported HTTP status");
+        };
+    }
+
+    /**
+     * 
+     * @param <T>        generico que nos permira saber que tipo de dato sera
+     *                   retornado en el cuerpo de la peticion
+     * @param client_id  id del cliente en la base de datos
+     * @param projects   proyecto el cual se le asignara a un cliente ya sea agregar
+     *                   modificar eliminar etc
+     * @param httpStatus status code que sera retornado al cliente ej: 200, 203, 404
+     *                   , 500 etc.
+     * @param returnType mensaje de retorno si las validaciones salieron correcto
+     * @param error      mensaje de error si las validaciones retornaron bad request
+     * @return retorna error / cuerpo - returntype / cuerpo
+     */
+    public static <T> ResponseEntity<T> Validate(UUID client_id, Projects projects, HttpStatus httpStatus,
+            T returnType,
+            T error) {
+        if (client_id == null)
+            return ResponseEntity.badRequest().body(error);
+
+        if (projects == null)
+            return ResponseEntity.badRequest().body(error);
+
+        return switch (httpStatus) {
+            case CREATED -> ResponseEntity.status(HttpStatus.CREATED).body(returnType);
+            case OK -> ResponseEntity.ok(returnType);
+            default -> throw new IllegalArgumentException("Unsupported HTTP status");
+        };
+    }
+
+    /**
+     * 
+     * @param <T>        generico que nos permira saber que tipo de dato sera
+     *                   retornado en el cuerpo de la peticion
+     * @param client_id  id del cliente en la base de datos
+     * @param id         id del proyecto en la base de datos
+     * @param projects   proyecto el cual se le asignara a un cliente ya sea agregar
+     *                   modificar eliminar etc
+     * @param httpStatus status code que sera retornado al cliente ej: 200, 203, 404
+     *                   , 500 etc.
+     * @param returnType mensaje de retorno si las validaciones salieron correcto
+     * @param error      mensaje de error si las validaciones retornaron bad request
+     * @return retorna error / cuerpo - returntype / cuerpo
+     */
+    public static <T> ResponseEntity<T> Validate(UUID client_id, Integer id, Projects projects, HttpStatus httpStatus,
+            T returnType,
+            T error) {
+        if (client_id == null)
+            return ResponseEntity.badRequest().body(error);
+
+        if (projects == null)
+            return ResponseEntity.badRequest().body(error);
+
+        if (!(id instanceof Integer))
+            return ResponseEntity.badRequest().body(error);
+
+        return switch (httpStatus) {
+            case CREATED -> ResponseEntity.status(HttpStatus.CREATED).body(returnType);
+            case OK -> ResponseEntity.ok(returnType);
+            default -> throw new IllegalArgumentException("Unsupported HTTP status");
+        };
     }
 }
