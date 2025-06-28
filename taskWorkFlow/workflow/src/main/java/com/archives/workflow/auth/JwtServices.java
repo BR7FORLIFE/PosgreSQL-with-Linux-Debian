0cf2bb88a15dde47a3ff66032be0d9be7ba5ec58
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-06-27 21:19:11 -0500

    add the methods for utils

--- taskWorkFlow/workflow/src/main/java/com/archives/workflow/auth/JwtServices.java
+++ taskWorkFlow/workflow/src/main/java/com/archives/workflow/auth/JwtServices.java
@@ -1,6 +1,32 @@
 package com.archives.workflow.auth;
 
+import org.springframework.stereotype.Component;
 
+import com.archives.workflow.auth.objectsAuth.RegisterAuth;
+
+import io.jsonwebtoken.Claims;
+
+@Component
 public class JwtServices {
-    
+    // <-- secret key for json web token
+
+    public String generateToken(RegisterAuth registerAuth) { // <-- generate json web token
+        return null;
+    }
+
+    public String extractJsonWebToken(String token) { // <-- extract json web token
+        return null;
+    }
+
+    public boolean isTokenValid(String token, RegisterAuth registerAuth) { // <-- valid the token for the user
+        return false;
+    }
+
+    private boolean isTokenExpired(String token) { // verified the token expired
+        return false;
+    }
+
+    private Claims extractAllClaims(String token) { // decode json web token and extract information for user
+        return null;
+    }
 }
