93b08561eea9b67d90292c049633d7979f408ffc
BR7FORLIFE <diazpachecobrayan@gmail.com>
2025-07-02 10:20:57 -0500

    install lombok dependency and update for this class

--- taskWorkFlow/workflow/src/main/java/com/archives/workflow/auth/ObjectAuth/AuthenticationResponse.java
+++ taskWorkFlow/workflow/src/main/java/com/archives/workflow/auth/ObjectAuth/AuthenticationResponse.java
@@ -1,35 +1,12 @@
 package com.archives.workflow.auth.ObjectAuth;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthenticationResponse {
-    public String username;
-    public String token;
-    public Date datetime;
-
-    public AuthenticationResponse(){}
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Date getDatetime() {
-        return datetime;
-    }
-
-    public void setDatetime(Date datetime) {
-        this.datetime = datetime;
-    }
+    private String token;
 }
