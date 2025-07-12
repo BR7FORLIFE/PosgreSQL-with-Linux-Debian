cfe896fab3faf4e8beecb4ee65c78cb6ce3c3fb5
br7forlife <diazpachecobrayan@gmail.com>
2025-07-12 12:07:09 -0500

    refactor

--- JWT/jwtproject/src/main/java/com/example/jwtproject/controllers/UserController.java
+++ JWT/jwtproject/src/main/java/com/example/jwtproject/controllers/UserController.java
@@ -1,14 +1,8 @@
 package com.example.jwtproject.controllers;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.jwtproject.models.User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.jwtproject.services.UserServices;
 
 import lombok.RequiredArgsConstructor;
@@ -19,8 +13,4 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     private final UserServices userServices;
 
-    @GetMapping("/user")
-    public ResponseEntity<List<User>> getAllUsers(){
-        return ResponseEntity.status(HttpStatus.OK).body(userServices.getUsers());
-    }
 }
