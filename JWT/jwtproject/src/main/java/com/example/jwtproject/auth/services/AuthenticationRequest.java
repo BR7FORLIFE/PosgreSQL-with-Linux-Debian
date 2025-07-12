d52fbbcb941245fbe0498619945ab8e3c8f9f4db
br7forlife <diazpachecobrayan@gmail.com>
2025-07-12 12:07:38 -0500

    change the name

--- JWT/jwtproject/src/main/java/com/example/jwtproject/auth/services/AuthenticationRequest.java
+++ JWT/jwtproject/src/main/java/com/example/jwtproject/auth/services/AuthenticationRequest.java
@@ -1,5 +1,14 @@
 package com.example.jwtproject.auth.services;
 
+import com.example.jwtproject.enums.Rol;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Data
 public class AuthenticationRequest {
-    
+    private final String username;
+    private final String password;
+    private final Rol rol;
 }
