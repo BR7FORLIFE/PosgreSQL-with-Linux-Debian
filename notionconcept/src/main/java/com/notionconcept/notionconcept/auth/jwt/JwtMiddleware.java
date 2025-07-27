0983031c61864c53a8f4ace2a45bf6e64495a17c
br7forlife <diazpachecobrayan@gmail.com>
2025-07-27 15:36:22 -0500

    add logic for jwt middleware for use in securityConfig

--- notionconcept/src/main/java/com/notionconcept/notionconcept/auth/jwt/JwtMiddleware.java
+++ notionconcept/src/main/java/com/notionconcept/notionconcept/auth/jwt/JwtMiddleware.java
@@ -1,5 +1,56 @@
 package com.notionconcept.notionconcept.auth.jwt;
 
-public class JwtMiddleware {
-    
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtMiddleware extends OncePerRequestFilter { // este middleware es el que nos permite acceder a los
+                                                          // servicios de jwt gracias a que lo definimos en el
+                                                          // securityConfig
+    private final JwtServices jwtServices;
+    private final UserDetailsService userDetailsService;
+
+    @SuppressWarnings("null")
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException, java.io.IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String username;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        username = jwtServices.extractUsername(jwt);
+
+        if(username == null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if(jwtServices.isValidToken(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+    }
 }
