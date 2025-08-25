513aba5746fcb0058b11eaef7ade54a7846f3de8
br7forlife <diazpachecobrayan@gmail.com>
2025-08-25 12:40:48 -0500

    create to controller for testing in postman

---
+++ HttpBasic/src/main/java/com/archives/HttpBasic/controllers/AuthController.java
@@ -0,0 +1,17 @@
+package com.archives.HttpBasic.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    
+    @PostMapping("/login")
+    public ResponseEntity<String> login(Authentication authentication){
+        return ResponseEntity.ok().body("Usuario registrado!" + authentication.getName());
+    }
+}
