20809b0492541c35c92dc9e6dd68679d6ca378a2
br7forlife <diazpachecobrayan@gmail.com>
2025-08-27 14:43:14 -0500

    change postmapping to getmapping

--- HttpBasic/src/main/java/com/archives/HttpBasic/controllers/AuthController.java
+++ HttpBasic/src/main/java/com/archives/HttpBasic/controllers/AuthController.java
@@ -2,7 +2,7 @@ package com.archives.HttpBasic.controllers;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
     
-    @PostMapping("/login")
+    @GetMapping("/login")
     public ResponseEntity<String> login(Authentication authentication){
-        return ResponseEntity.ok().body("Usuario registrado!" + authentication.getName());
+        return ResponseEntity.ok().body("Usuario registrado! " + authentication.getName() + authentication.getCredentials() + authentication.getAuthorities());
     }
 }
