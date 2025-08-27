3621b1d45cb3245ca27ecbaeab9a96ef6fc27808
br7forlife <diazpachecobrayan@gmail.com>
2025-08-27 14:43:53 -0500

    agg some configuration in securityConfig

--- HttpBasic/src/main/java/com/archives/HttpBasic/security/SecurityConfig.java
+++ HttpBasic/src/main/java/com/archives/HttpBasic/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.archives.HttpBasic.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,7 +15,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request -> request.requestMatchers("/login").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
