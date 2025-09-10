4cc6864af375f4efcafe513ce02f044a9403a087
br7forlife <diazpachecobrayan@gmail.com>
2025-09-10 09:04:11 -0500

    add formlogin and activate Oauth2 with github

--- OAuth2/OAuth2/src/main/java/com/archives/OAuth2/security/config/SecurityConfig.java
+++ OAuth2/OAuth2/src/main/java/com/archives/OAuth2/security/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.archives.OAuth2.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,7 +12,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        return http
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/public").permitAll().anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
+                .build();
     }
 }
